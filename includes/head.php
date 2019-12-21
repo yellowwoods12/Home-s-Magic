1a23e36100afd94d8cfd4b744511fc9731c4a2c1
Anindya Kundu <anindya.k22@outlook.com>
2019-12-21 23:51:01 +0530

    Separate out common code to external files

---
+++ includes/head.php
@@ -0,0 +1,34 @@
+<!-- HTML head common content -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+<!-- Bootstrap 4.4.1 minified Stylesheet -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<!-- Custom Stylesheet -->
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<!-- FontAwesome 5.12 CDN -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css" integrity="sha384-REHJTs1r2ErKBuJB0fCK99gCYsVjwxHrSU0N7I1zl9vZbggVJXRMsv/sLlOAGb4M" crossorigin="anonymous">
+
+<!-- Google Fonts -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Amatic+SC&display=swap">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Charmonman&display=swap">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Titillium+Web&display=swap">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Acme&display=swap">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato&display=swap">
+
+<!-- jQuery 3.4.1 minified Script -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" crossorigin="anonymous"></script>
+<!-- Bootstrap 4.0.0 minified plugin Script -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<!-- Custom Script for form validation and response -->
+<script type="text/javascript" src="js/save_details.js"></script>
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
---
+++ includes/signupModal.php
@@ -0,0 +1,60 @@
+<!-- modal for Sign Up-->
+<div id="signupModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="signupModal" aria-hidden="true">
+  <div class="modal-dialog modal-dialog centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h3 class="modal-title">Sign Up</h3>
+        <button type="button" class="close" data-dismiss="modal">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+      <div class="modal-body">
+        <form id="signupForm" method="POST">
+          <div class="form-group">
+            <label for="fname">First Name:</label>
+            <input type="text" class="form-control" id="fname" placeholder="Enter First Name">
+          </div>
+
+          <div class="form-group">
+            <label for="lname">Last Name:</label>
+            <input type="text" class="form-control" id="lname" placeholder="Enter last Name">
+          </div>
+
+          <div class="form-group">
+            <label for="mail">Email-id:</label>
+            <input type="email" class="form-control" id="mail" placeholder="Enter Email Id">
+          </div>
+
+          <div class="form-group">
+            <label for="phone">Phone number:</label>
+            <input type="number" class="form-control" id="phone" placeholder="Phone Number">
+          </div>
+
+          <div class="form-group">
+            <label for="Address">Address:</label>
+            <textarea name="address" id="address" class="form-control" cols="25" rows="5" placeholder="Enter your address"></textarea>
+          </div>
+
+          <div class="form-group">
+            <label for="pwd">Password:</label>
+            <input type="password" class="form-control" id="pwd" placeholder="Enter Password">
+          </div>
+
+          <div class="form-group">
+            <label for="cpwd">Confirm Password:</label>
+            <input type="password" class="form-control" id="cpwd" placeholder="Enter Password Again">
+          </div>
+
+          <div id="status"></div>
+        </form>
+      </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" onclick="register_details()">
+          Sign Up
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
